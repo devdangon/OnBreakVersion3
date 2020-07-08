ed4472233414afab2aea39a5efa7d4d4f43bc0cb
dandevgon <dangonzalezf@gmail.com>
2020-07-08 17:23:57 -0400

    Creacion de clases del patron Abstract Builder

---
+++ OnBreak_v3/BiblioteClases/IVegetariana.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnBreak
+{
+    public interface IVegetariana
+    {
+        bool AlimentacionVegetariana  { get; set; }
+    }
+}
